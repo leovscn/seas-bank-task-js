1ff75fbe0b0b59de920750291cddfbbdf5d5d3a0
Carolina Bastos Almeida <almeida.carolinabastos@gmail.com>
2023-03-02 19:42:45 -0300

    get e post comentários via api

--- assets/script.js
+++ assets/script.js
@@ -90,3 +90,46 @@ async function listarUsuarios() {
     });
 }
 listarUsuarios();
+
+const buscaComentarios = async()=>{
+  await fetch('https://my-json-server.typicode.com/leovscn/seas-bank-task-js/comments', {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response =>{
+    if(!response.ok){
+      throw new Error(`HTML response: ${response.status}`)
+    }
+    return response.json()
+  }).then(json => {
+    console.log(json)
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+buscaComentarios()
+
+const postaComentario = async ()=>{
+  await fetch('https://my-json-server.typicode.com/leovscn/seas-bank-task-js/comments',{
+    method: 'POST',
+    headers:{
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id:3,
+      body: 'thrid comment',
+      postId: 1
+    })
+  }).then(response =>{
+    if(!response.ok){
+      throw new Error(`HTML response: ${response.status}`)
+    }
+    return response.json()
+  }).then(data =>{
+    console.log(data)
+  }).catch(err =>{
+    console.log(err)
+  })
+}
+postaComentario()
